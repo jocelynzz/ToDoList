24328fd34e78e9b91300c092e4ff6709af2ab6b0
ZHAO ZHANG <jocelynz@uchicago.edu>
2015-01-20 16:09:47 -0600

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Jocelyn on 1/20/15.
+//  Copyright (c) 2015 Jocelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
